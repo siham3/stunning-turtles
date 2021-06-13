360c94f61a2c83452ea557a9b5406390ccf8874e
sihamali <siham.s18@hotmail.com>
2021-06-13 17:23:31 -0600

    Abo's Video

--- SihamOG/portfolio/main.js
+++ SihamOG/portfolio/main.js
@@ -5,3 +5,15 @@ new Vue({
 		show: true
 	}
 });
+
+function otherFunct()
+{
+	console.log("we are in another funct");
+	console.log("do some stuff");
+}
+
+console.log("Start: Hello my name is siham");
+
+otherFunct();
+
+console.log("End");
