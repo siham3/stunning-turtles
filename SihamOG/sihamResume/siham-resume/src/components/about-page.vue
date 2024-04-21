2386f7a962d804b69e639b6eb8d640ab524a1e37
sihamali <siham.s18@hotmail.com>
2024-04-21 17:33:17 -0600

    nav page

--- SihamOG/sihamResume/siham-resume/src/components/about-page.vue
+++ SihamOG/sihamResume/siham-resume/src/components/about-page.vue
@@ -1,28 +1,28 @@
 <template>
     <div class="about">
+        <nav-component />
         <div class="container">
             <div>
-                siham Salah
-            </div>
-            <div>haskdjhfkjasdhfkjashf <br/>
-                dasfasdfaskldfjlksadjf
-                asdflakjsdflkajsdflkj
+                hello
             </div>
         </div>
     </div>
 </template>
+<script setup lang="ts">
+import NavComponent from "./nav-component.vue";
+</script>
 
 <style scoped>
-.about{
-   align-items: center;
+.about {
+    align-items: center;
 }
-.container{
-    background-color: #76969B;
+.container {
+    background-color: #3E2A51;
     display: flex;
     justify-content: center;
     align-items: center;
     opacity: 60%;
     min-height: 80vh;
-    margin: 1%;
+    margin: 5% 12%;
 }
 </style>
---
+++ SihamOG/sihamResume/siham-resume/src/components/nav-component.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="header">
+        <h3>
+            Siham Salah
+        </h3>
+        <nav>
+            <a href="/About">about</a>
+            <a href="/">projects</a>
+            <a href="/About">art</a>
+        </nav>
+    </div>
+</template>
+<style scoped>
+h3 {
+    color: #2C0E00;
+    padding: 15px 80px 0 50px;
+    font-family: Cormorant;
+    font-weight: 800;
+    margin: 0;
+}
+
+a:hover {
+  color: #FFB890;
+}
+
+a {
+  color: #B08484;
+  font-family: Cormorant;
+  text-decoration: none;
+  font-weight: 800;
+  margin: 2%;
+  padding: 3%;
+}
+
+.header{
+    display: flex;
+    font-size: 40px;
+    align-items: center;
+    width:100%;
+    }
+</style>
