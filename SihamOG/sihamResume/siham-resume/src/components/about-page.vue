e4d4e7d587856cb8f9c2a6fe74479fe610928205
sihamali <siham.s18@hotmail.com>
2024-05-21 15:22:14 -0600

    Create tittle

--- SihamOG/sihamResume/siham-resume/src/components/about-page.vue
+++ SihamOG/sihamResume/siham-resume/src/components/about-page.vue
@@ -3,7 +3,14 @@
         <nav-component />
         <div class="container">
             <div>
-                hello
+            </div>
+            <div class="title">
+                <!-- <img src="./../assets/salam.png" alt="salams"> -->
+                <div id="title-text">
+                    Software Developer 
+                    <span id="and">&</span> 
+                    Professional Artist
+                </div>
             </div>
         </div>
     </div>
@@ -13,19 +20,44 @@ import NavComponent from "./nav-component.vue";
 </script>
 
 <style scoped>
-/* .about {
-    align-items: center;
-    justify-content: center;
-    max-width: 1200px;
-    min-width: 800px;
-} */
 .container {
-    background-color: #3E2A51;
+    /* background-color: #3E2A51; */
     display: flex;
     justify-content: center;
-    align-items: center;
-    opacity: 60%;
     min-height: 80vh;
-    margin: 5% 12%;
+    margin: 0 12%;
+    flex-direction: column;
+}
+
+#and {
+    color: #F9CE7A;
+}
+
+#title-text {
+    color: white;
+    font-family: Cormorant;
+    font-weight: 650;
+    font-size: 80px;
+    text-align: left;
+    line-height: 75px;
+    letter-spacing: 0px;
+    padding: 40px 20px;
+
+    background-image: url('./../assets/salam.png');
+    background-position: left -20px;
+    background-size: 190px;
+    background-repeat: no-repeat;
+
+    border-bottom: solid;
+    border-width: 2px;
+    border-block-color: #F9CE7A;
+
+    @media (max-width: 530px) {
+        font-size: 40px;
+        line-height: 40px;
+        background-size: 150px;
+        padding: 30px 20px;
+        background-position: left -18px;
+    }
 }
 </style>
